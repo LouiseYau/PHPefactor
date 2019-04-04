43e93509cfe9203c28a92010cf45d44fcea2abf5
LouiseYau <43237563+LouiseYau@users.noreply.github.com>
2019-04-04 16:12:45 +1300

    added new postgressdb db.php config  details to heroku

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "ec2-54-228-243-238.eu-west-1.compute.amazonaws.com";
-$user = "diadldlkicwrrr";
-$password = "629b14b92503e6c97b3b8474057f9279c63381222333b848ca2cc51cb56ead52";
-$dbname = "d2ph88enfggjhe";
+$host = "ec2-54-197-232-203.compute-1.amazonaws.com";
+$user = "mzczddtnpbehwl";
+$password = "df9a4cf4f89c91788a80ba46ca2906d73a2f005fe212d060202db05396d5439b";
+$dbname = "d29chcvsqfn3rt";
 $port = "5432";
 try{
   //Set DSN data source name
